943355650d79ee985cba5c4a260694741e10a6de
Saleem Mansour <125513026+salim0mans@users.noreply.github.com>
2023-11-09 20:08:21 +0300

    Update liftover.sh

--- Scripts/liftover.sh
+++ Scripts/liftover.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-CrossMap.py vcf GRCh37_to_GRCh38.chain.gz input.vcf.gz Homo_sapiens.GRCh38.dna.primary_assembly.fa output.vcf
+CrossMap.py vcf --chromid l GRCh37_to_GRCh38.chain.gz input.vcf.gz Homo_sapiens.GRCh38.dna.primary_assembly.fa output.vcf
